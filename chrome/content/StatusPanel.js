d48770c26a7603ef93b8c5bb457df423a8e54804
Heraldo Carneiro <heraldo@heraldocarneiro.com>
2009-10-03 16:32:50 -0300

    Primeiro JS

---
+++ chrome/content/StatusPanel.js
@@ -0,0 +1,16 @@
+if (!com) var com = {};
+if (!com.heraldocarneiro) com.heraldocarneiro = {};
+if (!com.heraldocarneiro.bonner) com.heraldocarneiro.bonner = {};
+
+
+com.heraldocarneiro.bonner.StatusPanel = function() {
+
+var oi = 'oi';
+
+return {
+	onClick: function(event) {
+		alert(oi);
+	}
+};
+
+}();
